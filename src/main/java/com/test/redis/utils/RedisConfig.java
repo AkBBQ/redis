b7a4697650439a6c902772594f0cf3c62f7409b1
sunjie <15251800012@163.com>
2019-06-13 16:59:30 +0800

    提交

---
+++ src/main/java/com/test/redis/utils/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.test.redis.utils;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * All rights Reserved, Designed By www.maihaoche.com
+ *
+ * @Package com.test.redis.utils
+ * @author: angtai（angtai@maihaoche.com）
+ * @date: 2019/4/2 5:13 PM
+ * @Copyright: 2017-2020 www.maihaoche.com Inc. All rights reserved.
+ */
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(factory);
+        return redisTemplate;
+    }
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(factory);
+        return stringRedisTemplate;
+    }
+
+}
