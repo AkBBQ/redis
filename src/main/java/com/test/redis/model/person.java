84ada36c4be161e0cce899e44141839980fafb3a
sunjie <15251800012@163.com>
2019-06-25 18:16:31 +0800

    redis服务搭建完成

---
+++ src/main/java/com/test/redis/model/person.java
@@ -0,0 +1,10 @@
+package com.test.redis.model;
+
+import lombok.Data;
+
+@Data
+public class person {
+    private Integer id;
+    private String name;
+    private Integer age;
+}
--- src/test/java/com/test/redis/RedisApplicationTests.java
+++ src/test/java/com/test/redis/RedisApplicationTests.java
@@ -1,7 +1,9 @@
 package com.test.redis;
 
+import com.test.redis.cache.RedisService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class RedisApplicationTests {
 
+	@Autowired
+	private RedisService redisService;
+
 	@Test
 	public void contextLoads() {
+		//从redis中读取key
+		String name = redisService.getString("cat1");
+		System.out.println(name);
+		redisService.add("sunjie","sunjie");
 	}
 
 }
